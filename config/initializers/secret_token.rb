654cf8631aa07ff1f4b24c0ac2c68b0384e3773c
The Capitol <capitol@The-Capitol.local>
2013-11-03 15:03:27 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '78b5411b1e7aa1bf8aed62f1654382de9770846610a5e3b13052fd3654f51dca9c645a112743542100af52e85acd41d0a7a789564583c01adec68ee93db3e0f5'
